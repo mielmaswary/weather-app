50a742e9f55b612caba8a24f2853bcf2ce1f8122
mielmaswary <miel1983@gmail.com>
2021-02-03 20:54:52 +0200

    set PORT in src/app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
